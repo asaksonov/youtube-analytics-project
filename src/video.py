4ea2c69cdecce6623bd17519d15cc4614cf83cad
Andrey Saksonov <asaksonov@gmail.com>
2023-08-26 22:39:51 +0300

    add try/except

--- src/video.py
+++ src/video.py
@@ -5,15 +5,18 @@ class Video:
 
     def __init__(self, video_id):
         self.video_id = video_id
-        self.fetch_video_info()
-
-    def fetch_video_info(self):
-        service = build('youtube', 'v3', developerKey="AIzaSyBaOVsm5nVNANKOv0JL_joAFxW0a6Zm9QE")
-        video_response = service.videos().list(part='snippet, statistics', id=self.video_id).execute()
-        self.title = video_response['items'][0]['snippet']['title']
-        self.video_link = f"https://youtu.be/{self.video_id}"
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+        try:
+            service = build('youtube', 'v3', developerKey="AIzaSyBaOVsm5nVNANKOv0JL_joAFxW0a6Zm9QE")
+            video_response = service.videos().list(part='snippet, statistics', id=self.video_id).execute()
+            self.title = video_response['items'][0]['snippet']['title']
+            self.video_link = f"https://youtu.be/{self.video_id}"
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.video_link = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.title
